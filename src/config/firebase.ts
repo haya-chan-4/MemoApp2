bd24dac26fff930e223ba647884fd21a397bed3f
Hayata <hayata8812@gmail.com>
2025-03-10 10:50:29 +0900

    環境変数の設定

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,14 @@ import { getFirestore } from 'firebase/firestore'
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGchCubXovU3hGPr7GfGbgoNKIMRI26Zc",
-  authDomain: "memoapp-ecc9a.firebaseapp.com",
-  projectId: "memoapp-ecc9a",
-  storageBucket: "memoapp-ecc9a.firebasestorage.app",
-  messagingSenderId: "506635628211",
-  appId: "1:506635628211:web:786ab6a27678f2d3850ac6"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
+
 
 // firebaseのアプリの初期化
 const app = initializeApp(firebaseConfig)
