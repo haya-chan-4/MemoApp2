2bf3a90d2305e4a02a1b5c45ee770ec44431546b
Hayata <hayata8812@gmail.com>
2025-03-10 10:38:44 +0900

    firebaseの設定

---
+++ src/config/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app'
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGchCubXovU3hGPr7GfGbgoNKIMRI26Zc",
+  authDomain: "memoapp-ecc9a.firebaseapp.com",
+  projectId: "memoapp-ecc9a",
+  storageBucket: "memoapp-ecc9a.firebasestorage.app",
+  messagingSenderId: "506635628211",
+  appId: "1:506635628211:web:786ab6a27678f2d3850ac6"
+}
+
+// firebaseのアプリの初期化
+const app = initializeApp(firebaseConfig)
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+})
+const db = getFirestore(app)
+export { app, auth, db }
