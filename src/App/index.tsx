ec199b8f4fc481c3dbcd192ad0b4153239e4e6a1
Hayata <hayata8812@gmail.com>
2025-03-10 19:55:09 +0900

    ログイン状態の監視機能

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,8 +1,18 @@
-import { Redirect } from 'expo-router'
+import { Redirect, router } from 'expo-router'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../config/firebase'
+import { useEffect } from 'react'
 
 const Index = (): JSX.Element => {
-  return(
-    <Redirect href="/auth/sign_up"/>
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) =>{
+      if(user !== null){
+        router.replace('/memo/list')
+      }
+    })
+  }, []);
+  return (
+    <Redirect href="/auth/sign_up" />
   )
 }
 
